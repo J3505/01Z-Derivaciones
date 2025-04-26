07d4bdea83347613cbe19b1343607857ae39dcc5
Jason <alonstarkj@gmail.com>
2025-04-25 22:59:08 -0500

    feat: Se añadio iconos principales y actualizar estilos en todos los componentes
    - Se añadió la dependencia de iconos principales a package.json y package-lock.json.
    - Se actualizó dashboard.component.html para cambiar el esquema de color de naranja a azul.
    - Se refactorizó derivacion.component.html para mejorar el diseño con estadísticas y la visualización de elementos.
    - Se actualizaron los estilos en derivacion.component.scss para mejorar los efectos al pasar el cursor e indicadores de estado.
    - Se modificaron los componentes de encabezado y barra lateral para usar el esquema de color azul en lugar de naranja.
    - Se corrigió login.component.html para garantizar un estilo de entrada consistente.
    - Se limpió estudiantes.component.html eliminando enlaces redundantes.
    - Se aseguró la importación de iconos principales en designs.css para su uso en toda la aplicación.

--- src/app/admin/derivacion/derivacion.component.ts
+++ src/app/admin/derivacion/derivacion.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 
 
 @Component({
   selector: 'app-derivacion',
-  imports: [],
+  imports: [Dialog, ButtonModule, InputTextModule, CommonModule, FormsModule],
   templateUrl: './derivacion.component.html',
   styleUrl: './derivacion.component.scss'
 })
 export default class DerivacionComponent {
+  
+  visible: boolean = false;
+  showDialog() {
+    this.visible = true;
+}
 
 }
